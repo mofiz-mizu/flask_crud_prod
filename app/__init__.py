06816575c1b653fa0537c892b240d748ed37deae
mofiz-mizu <haquemizu@gmail.com>
2025-12-19 02:12:51 +0600

    init edit

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.config import Config
 from app.extensions import db
 from app.users.routes import bp
 import time
+from sqlalchemy.exc import OperationalError
 
 def create_app():
     app = Flask(__name__)
@@ -11,12 +12,21 @@ def create_app():
     db.init_app(app)
     app.register_blueprint(bp)
 
+    # with app.app_context():
+    #     for _ in range(10):
+    #         try:
+    #             db.create_all()
+    #             break
+    #         except Exception:
+    #             time.sleep(2)
     with app.app_context():
-        for _ in range(10):
-            try:
-                db.create_all()
-                break
-            except Exception:
-                time.sleep(2)
+    retries = 10
+    while retries > 0:
+        try:
+            db.create_all()
+            break
+        except OperationalError:
+            retries -= 1
+            time.sleep(2)
 
     return app
